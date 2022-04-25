a030547305bad9b2b889df41b14b311e92e0cbcd
a_szczepanik <a.szczepanik@adform.com>
2022-04-25 10:15:53 +0200

    Add method partitionCount

---
+++ src/main/scala/PartitionCount.scala
@@ -0,0 +1,24 @@
+import scala.annotation.tailrec
+
+object PartitionCount {
+
+  def partitionCount(n: Int): Int = {
+
+    @tailrec
+    def countUp(amount: Int, seq: List[Int]): List[Int] = (amount, seq) match {
+      case (amount, seq) if (amount < seq.head) => seq.head + amount :: seq.tail
+      case _ => countUp(amount - seq.head, seq.head :: seq)
+    }
+
+    def advance(partition: Option[List[Int]]): Option[List[Int]] = {
+      partition.get match {
+        case List(n) => None
+        case p => Some(countUp(p.head - 1, p.tail.head + 1 :: p.tail.tail))
+      }
+    }
+
+    Iterator.iterate(Some(List.fill(n)(1)): Option[List[Int]])(advance)
+      .takeWhile(_.isDefined)
+      .map(_.get).size
+  }
+}
--- src/test/scala/PartitionCountSpec.scala
+++ src/test/scala/PartitionCountSpec.scala
@@ -1,13 +1,20 @@
+import PartitionCount._
 import org.scalatest._
 import flatspec._
 import matchers._
 
 class PartitionCountSpec extends AnyFlatSpec with should.Matchers {
 
-  def partitionCount(n: Int): Int = if (n == 5) 7 else 6
-
   "partitionCount" should "return partition of number 5 as the sum of positive integers" in {
     partitionCount(5) should equal(7)
-    partitionCount(4) should equal(6)
   }
+
+  it should "return partition of number 4 as the sum of positive integers" in {
+    partitionCount(4) should equal(5)
+  }
+
+  it should "return partition of number 666 as the sum of positive integers" in {
+    partitionCount(66) should equal(2323520)
+  }
+
 }
